832b8432b2596420a119a1c4f4985e43b1bef350
Alex Merced - AlexMercedCoder.com <alexmerced@alexmerced.com>
2019-12-16 21:23:21 -0500

    updates

--- src/components.d.ts
+++ src/components.d.ts
@@ -10,17 +10,6 @@ import { HTMLStencilElement, JSXBase } from '@stencil/core/internal';
 
 
 export namespace Components {
-  interface AmBlog {}
-  interface AmCard {
-    'cardImage': string;
-    'cardtitle': string;
-    'link1': string;
-    'link2': string;
-    'linkText1': string;
-    'linkText2': string;
-  }
-  interface AmResume {}
-  interface AmWpBlog {}
   interface AppHome {}
   interface AppProfile {}
   interface AppRoot {}
@@ -28,30 +17,6 @@ export namespace Components {
 
 declare global {
 
-
-  interface HTMLAmBlogElement extends Components.AmBlog, HTMLStencilElement {}
-  var HTMLAmBlogElement: {
-    prototype: HTMLAmBlogElement;
-    new (): HTMLAmBlogElement;
-  };
-
-  interface HTMLAmCardElement extends Components.AmCard, HTMLStencilElement {}
-  var HTMLAmCardElement: {
-    prototype: HTMLAmCardElement;
-    new (): HTMLAmCardElement;
-  };
-
-  interface HTMLAmResumeElement extends Components.AmResume, HTMLStencilElement {}
-  var HTMLAmResumeElement: {
-    prototype: HTMLAmResumeElement;
-    new (): HTMLAmResumeElement;
-  };
-
-  interface HTMLAmWpBlogElement extends Components.AmWpBlog, HTMLStencilElement {}
-  var HTMLAmWpBlogElement: {
-    prototype: HTMLAmWpBlogElement;
-    new (): HTMLAmWpBlogElement;
-  };
 
   interface HTMLAppHomeElement extends Components.AppHome, HTMLStencilElement {}
   var HTMLAppHomeElement: {
@@ -71,10 +36,6 @@ declare global {
     new (): HTMLAppRootElement;
   };
   interface HTMLElementTagNameMap {
-    'am-blog': HTMLAmBlogElement;
-    'am-card': HTMLAmCardElement;
-    'am-resume': HTMLAmResumeElement;
-    'am-wp-blog': HTMLAmWpBlogElement;
     'app-home': HTMLAppHomeElement;
     'app-profile': HTMLAppProfileElement;
     'app-root': HTMLAppRootElement;
@@ -82,26 +43,11 @@ declare global {
 }
 
 declare namespace LocalJSX {
-  interface AmBlog {}
-  interface AmCard {
-    'cardImage'?: string;
-    'cardtitle'?: string;
-    'link1'?: string;
-    'link2'?: string;
-    'linkText1'?: string;
-    'linkText2'?: string;
-  }
-  interface AmResume {}
-  interface AmWpBlog {}
   interface AppHome {}
   interface AppProfile {}
   interface AppRoot {}
 
   interface IntrinsicElements {
-    'am-blog': AmBlog;
-    'am-card': AmCard;
-    'am-resume': AmResume;
-    'am-wp-blog': AmWpBlog;
     'app-home': AppHome;
     'app-profile': AppProfile;
     'app-root': AppRoot;
@@ -114,10 +60,6 @@ export { LocalJSX as JSX };
 declare module "@stencil/core" {
   export namespace JSX {
     interface IntrinsicElements {
-      'am-blog': LocalJSX.AmBlog & JSXBase.HTMLAttributes<HTMLAmBlogElement>;
-      'am-card': LocalJSX.AmCard & JSXBase.HTMLAttributes<HTMLAmCardElement>;
-      'am-resume': LocalJSX.AmResume & JSXBase.HTMLAttributes<HTMLAmResumeElement>;
-      'am-wp-blog': LocalJSX.AmWpBlog & JSXBase.HTMLAttributes<HTMLAmWpBlogElement>;
       'app-home': LocalJSX.AppHome & JSXBase.HTMLAttributes<HTMLAppHomeElement>;
       'app-profile': LocalJSX.AppProfile & JSXBase.HTMLAttributes<HTMLAppProfileElement>;
       'app-root': LocalJSX.AppRoot & JSXBase.HTMLAttributes<HTMLAppRootElement>;
